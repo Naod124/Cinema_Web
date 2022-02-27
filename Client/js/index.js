4d80bf0ce1704f983066d6395cca1c3b8a67dd96
kristijan_00 <kristijandjakovic10@gmail.com>
2022-02-27 15:45:37 +0100

    Added the header + css styles and animation

--- Client/js/index.js
+++ Client/js/index.js
@@ -4,3 +4,15 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+const buttonTrailer = document.querySelector("button");
+const buttonTrailerPlay = document.querySelector("i");
+buttonTrailer.addEventListener('mouseover', () => {
+  buttonTrailer.classList.add('button-darken')
+  buttonTrailerPlay.classList.add('button-darken')
+})
+
+buttonTrailer.addEventListener('mouseout', () => {
+  buttonTrailer.classList.remove('button-darken')
+  buttonTrailerPlay.classList.remove('button-darken')
+})
