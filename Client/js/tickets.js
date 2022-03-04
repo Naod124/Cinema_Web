0037c5995a0c40fbca8ca3432e50fe3cb59b50b4
Peter Daniel <petrus.daniel@protonmail.com>
2022-03-04 02:26:20 +0100

    Add a logic for selecting seats  + Move the css rule for ticket.html to a separate css file

---
+++ Client/js/tickets.js
@@ -0,0 +1,23 @@
+const price = document.getElementById("price");
+const seatContainer = document.querySelector(".seat-container");
+const seats = document.querySelectorAll(".row .seat:not(.taken)");
+const cart = document.querySelector(".fa-shopping-cart");
+
+const ticketPrice = +price.value;
+
+function updatedSelectedSeat() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const numberOfSelectedSeats = selectedSeats.length;
+  // cart.innerHTML = numberOfSelectedSeats * ticketPrice;
+}
+
+seatContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("taken")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updatedSelectedSeat();
+  }
+});
