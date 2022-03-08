be09c93177af9f0fd27ee9ef3ce9f2095c481de4
Naod <Naoderi36@gmail.com>
2022-03-08 16:35:49 +0100

    New database connection

--- Server/DB/Connect.js
+++ Server/DB/Connect.js
@@ -4,7 +4,8 @@ const connectedKnex = knex({
 client: "sqlite3",
 connection: {
     filename: "Filmvisarna.sqlite3"
-}
+}, 
+useNullAsDefault: true
 }); 
 
 module.exports = connectedKnex; 
--- Server/app.js
+++ Server/app.js
@@ -2,20 +2,45 @@ const express = require("express");
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser'); 
 const db = require("./DB/Queries"); 
+const { json } = require("body-parser");
 
 
+let tempName =''; 
+let tempPass =''; 
 const app = express(); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.post('/login',function name(req, res) {
-    let name = req.body.username; 
-    let password = req.body.password; 
-    if(db.getUser(name, password)==1){
-        res.send('0'); 
-    }
+
+
+
+app.post('/login',function (request, response) {
+    //console.log(db.getUser('Naoderi36@gmail.com', '12345'));
+    var username = request.query.username; 
+    var password = request.query.password; 
+
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+        var db= new sqlite3.Database('/Users/tekie/Desktop/Cinema_Web/Filmvisarna.sqlite3',(err)=>{
+            if(!err){
+                db.all('SELECT * FROM Customers where username="'+username+'" and password="'+password+'"',(err,result)=>{
+                    if(result.length==1){
+                        return response.send('1')
+                    }
+                    else{
+                        return response.send('0')
+                    }
+                });
+            }
+         });
+         
 }); 
 
-app.listen(8080); 
+app.get('/hello',function name(req, res){
+res.send('hello Cinema'); 
+
+});
+
+app.listen(7777); 
+module.exports = app;
