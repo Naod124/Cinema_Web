bc9a000362449e45b1ae970c8908f87efa02a53c
kristijan_00 <kristijandjakovic10@gmail.com>
2022-03-13 22:02:15 +0100

    Ticket buying is almost fully functional

--- Client/js/signIn.js
+++ Client/js/signIn.js
@@ -1,145 +1,146 @@
 
-  function submitClick(){
-    let username = $('#username').val();
-    let password = $('#password').val();
-    let jsonData = JSON.stringify({
-        'username':username, 'password':password
-    }); 
-    const url =  "http://localhost:7777/login"; 
+function submitClick() {
+  let username = $('#username').val();
+  let password = $('#password').val();
+  let jsonData = JSON.stringify({
+    'username': username, 'password': password
+  });
+  const url = "http://localhost:7777/login";
 
-    $.ajax({
-        
-        type: 'POST',
-        contentType: "application/json; charset=utf-8", 
-        url: url,
-        data: jsonData,
-        cache: false, 
-        dataType: "json",
-        jsonp: false,
-        success: function (data, textStatus, jqXHR) {
-            if(data==0){
-                alert("Invalid log in information. Try again")
-                $('#username').val('');
-                $('#password').val('');
+  $.ajax({
 
-            }
-            else if(data==1) {
-                alert('Logged in')
-            }
-        }
-    });
-  }
+    type: 'POST',
+    contentType: "application/json; charset=utf-8",
+    url: url,
+    data: jsonData,
+    cache: false,
+    dataType: "json",
+    jsonp: false,
+    success: function (data, textStatus, jqXHR) {
+      if (data == 0) {
+        alert("Invalid log in information. Try again")
+        $('#username').val('');
+        $('#password').val('');
 
-  function openForm() {
-    $("#myForm").show();
-  }
-  
-  function closeForm() {
-$("#myForm").hide(); 
-$("#myForm1").hide();
-$("#myForm2").hide(); }
+      }
+      else if (data == 1) {
+        alert('Logged in')
+      }
+    }
+  });
+}
+
+function openForm() {
+  $("#myForm").show();
+}
+
+function closeForm() {
+  $("#myForm").hide();
+  $("#myForm1").hide();
+  $("#myForm2").hide();
+}
 
 
 
- function secondForm() {
+function secondForm() {
   let email = $('#email').val();
- 
+
 
   let jsonData = JSON.stringify({
-      'email':email
-  }); 
-  const url =  "http://localhost:7777/forgetPass"; 
-  
-        
-  $.ajax({  
+    'email': email
+  });
+  const url = "http://localhost:7777/forgetPass";
+
+
+  $.ajax({
     type: 'POST',
-    contentType: "application/json; charset=utf-8", 
+    contentType: "application/json; charset=utf-8",
     url: url,
     data: jsonData,
-    cache: false, 
+    cache: false,
     dataType: "json",
     jsonp: false,
     success: function (data, textStatus, jqXHR) {
-      
-        if(data==0){
-            alert("Invalid email")
-            $('#email').val('');
-            
-          }
-        else if(data==1) {
-          $("#myForm1").show();
-        }
+
+      if (data == 0) {
+        alert("Invalid email")
+        $('#email').val('');
+
+      }
+      else if (data == 1) {
+        $("#myForm1").show();
+      }
     }
-});
+  });
 
-    
- }
 
- function thirdForm() {
+}
+
+function thirdForm() {
   let resetCode = $('#resetCode').val();
- 
+
 
   let jsonData = JSON.stringify({
-      'resetCode':resetCode 
-  }); 
-  const url =  "http://localhost:7777/resetCode"; 
-  
-        
-  $.ajax({  
+    'resetCode': resetCode
+  });
+  const url = "http://localhost:7777/resetCode";
+
+
+  $.ajax({
     type: 'POST',
-    contentType: "application/json; charset=utf-8", 
+    contentType: "application/json; charset=utf-8",
     url: url,
     data: jsonData,
-    cache: false, 
+    cache: false,
     dataType: "json",
     jsonp: false,
     success: function (data, textStatus, jqXHR) {
-      
-        if(data==0){
-            alert("Invalid reset code")
-            $('#resetCode').val('');
-            
-          }
-        else if(data==1) {
-          $("#myForm").hide();
-          $("#myForm1").hide();
-          $("#myForm4").show();
-        }
-    }
-});
-    
- }
 
- function fourthForm() {
+      if (data == 0) {
+        alert("Invalid reset code")
+        $('#resetCode').val('');
+
+      }
+      else if (data == 1) {
+        $("#myForm").hide();
+        $("#myForm1").hide();
+        $("#myForm4").show();
+      }
+    }
+  });
+
+}
+
+function fourthForm() {
   let oldPassword = $('#oldPassword').val();
   let newPassword = $('#newPassword').val();
- 
+
 
   let jsonData = JSON.stringify({
-    'oldPassword':oldPassword, 'newPassword':newPassword
-}); 
-  const url =  "http://localhost:7777/newPass"; 
-  
-        
-  $.ajax({  
+    'oldPassword': oldPassword, 'newPassword': newPassword
+  });
+  const url = "http://localhost:7777/newPass";
+
+
+  $.ajax({
     type: 'POST',
-    contentType: "application/json; charset=utf-8", 
+    contentType: "application/json; charset=utf-8",
     url: url,
     data: jsonData,
-    cache: false, 
+    cache: false,
     dataType: "json",
     jsonp: false,
     success: function (data, textStatus, jqXHR) {
-      
-        if(data==0){
-            alert("Invalid reset code")
-            $('#password').val('');
-            $('#password1').val('');
-            
-          }
-        else if(data==1) {
-          alert('Password updated')
-        }
+
+      if (data == 0) {
+        alert("Invalid reset code")
+        $('#password').val('');
+        $('#password1').val('');
+
+      }
+      else if (data == 1) {
+        alert('Password updated')
+      }
     }
-});
- }
+  });
+}
--- Client/js/tickets.js
+++ Client/js/tickets.js
@@ -5,16 +5,19 @@ const price = document.getElementById("price"),
   seatCount = document.getElementById("seat-count"),
   totalPrice = document.getElementById("total-price"),
   shoppingCart = document.getElementById("shopping-cart"),
-  closeBtn = document.querySelector(".close-btn");
+  closeBtn = document.querySelector(".close-btn"),
+  buyBtn = document.querySelector(".buy-btn");
 
 let ticketPrice = +price.value;
+let selectedSeats = [];
 
 function updateSelectedSeat() {
-  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  selectedSeats = document.querySelectorAll(".row .seat.selected");
   const seatsIndex = [...selectedSeats].map(function (seat) {
     return [...seats].indexOf(seat);
   });
-  // console.log(seatsIndex);
+
+  console.log(seatsIndex);
   localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const numberOfSelectedSeats = selectedSeats.length;
@@ -43,25 +46,39 @@ closeBtn.addEventListener("click", () => {
   shoppingCart.classList.remove("show");
 });
 
+buyBtn.addEventListener("click", () => {
+  shoppingCart.classList.remove("show");
+  if (totalPrice.innerText > 0) {
+    saveTicketToDb();
+  } else {
+    alert("No seats selected!");
+  }
+});
+
 price.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
-  setTicketData(e.target.selectedIndex, e.target.value);
+  setTicketData(e.target.selectedIndex, e.target.value, undefined);
   updateSelectedSeat();
 });
 
-function setTicketData(ticketTypeIndex, ticketCost) {
+function setTicketData(ticketTypeIndex, ticketCost, reserved) {
   localStorage.setItem("selectedTicketTypeIndex", ticketTypeIndex);
   localStorage.setItem("selectedTicketPrice", ticketCost);
+  localStorage.setItem("reservedPlace", reserved);
 }
 
 function populateUIFromLocalStorage() {
-  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
   console.log(selectedSeats);
   if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
         seat.classList.add("selected");
       }
+
+      // if(!selectedSeats.indexOf(index) === undefined) {
+      //   seat.classList.add("taken");
+      // }
     });
   }
   const selectedTicketTypeIndex = localStorage.getItem(
@@ -77,6 +94,55 @@ function populateUIFromLocalStorage() {
   if (selectedTicketPrice !== null) {
     price.value = selectedTicketPrice;
   }
+}
+
+
+function saveTicketToDb() {
+  const url = "http://localhost:7777/tickets";
+
+  today = new Date();
+  // console.log(selectedSeats);
+  const seatsIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+
+  let total = totalPrice.innerText;
+  let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  let seatNum = seatsIndex.join("-");
+
+  let jsonData = JSON.stringify({
+    'date': date, 'seatNum': seatNum, 'totalPrice': total
+  });
+
+  $.ajax({
+
+    type: 'POST',
+    contentType: "application/json; charset=utf-8",
+    url: url,
+    data: jsonData,
+    cache: false,
+    dataType: "json",
+    jsonp: false,
+    success: function (data, textStatus, jqXHR) {
+      if (data == 0) {
+        console.log("Failed");
+      }
+      else if (data == 1) {
+        console.log("Success");
+        alert("Ticket bought successfully!");
+        for (i = 0; i < selectedSeats.length; i++) {
+          selectedSeats[i].classList.add('taken');
+          selectedSeats[i].classList.remove('selected');
+          // setTicketData(ticketTypeIndex, ticketCost, "taken");
+        }
+
+        totalPrice.innerText = 0;
+        selectedSeats.length = 0;
+        localStorage.clear();
+      }
+    }
+  });
+
 }
 
 populateUIFromLocalStorage();
