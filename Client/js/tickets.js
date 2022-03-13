e03c582df8f9dcf67dab23c59e10d1bb971ea178
Peter Daniel <petrus.daniel@protonmail.com>
2022-03-13 14:45:14 +0100

    Added a logic for saving the selected seats, ticket type and total price in local storage and populating the UI

--- Client/js/tickets.js
+++ Client/js/tickets.js
@@ -11,9 +11,17 @@ let ticketPrice = +price.value;
 
 function updateSelectedSeat() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const seatsIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+  // console.log(seatsIndex);
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const numberOfSelectedSeats = selectedSeats.length;
   seatCount.innerText = numberOfSelectedSeats;
   totalPrice.innerText = numberOfSelectedSeats * ticketPrice;
+
+  setTicketData(price.selectedIndex, price.value);
 }
 
 seatContainer.addEventListener("click", (e) => {
@@ -37,5 +45,39 @@ closeBtn.addEventListener("click", () => {
 
 price.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setTicketData(e.target.selectedIndex, e.target.value);
   updateSelectedSeat();
 });
+
+function setTicketData(ticketTypeIndex, ticketCost) {
+  localStorage.setItem("selectedTicketTypeIndex", ticketTypeIndex);
+  localStorage.setItem("selectedTicketPrice", ticketCost);
+}
+
+function populateUIFromLocalStorage() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  console.log(selectedSeats);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+  const selectedTicketTypeIndex = localStorage.getItem(
+    "selectedTicketTypeIndex"
+  );
+
+  if (selectedTicketTypeIndex !== null) {
+    price.selectedIndex = selectedTicketTypeIndex;
+  }
+
+  const selectedTicketPrice = localStorage.getItem("selectedTicketPrice");
+
+  if (selectedTicketPrice !== null) {
+    price.value = selectedTicketPrice;
+  }
+}
+
+populateUIFromLocalStorage();
+updateSelectedSeat();
